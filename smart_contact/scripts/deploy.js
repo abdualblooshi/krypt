b06d57739bfd2eb3beea42b28c89fcc95ec621fc
gitanimous <aboodshadow@outlook.com>
2021-12-30 01:31:26 +0400

    Fixed deploy.js

--- smart_contact/scripts/deploy.js
+++ smart_contact/scripts/deploy.js
@@ -1,5 +1,7 @@
 const main = async () => {
-  const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionsFactory = await hre.ethers.getContractFactory(
+    "Transactions"
+  );
   const transactionsContract = await transactionsFactory.deploy();
 
   await transactionsContract.deployed();
