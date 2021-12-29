7c874f46b344e19b4e8301c8fa5d154ce583d5ec
gitanimous <aboodshadow@outlook.com>
2021-12-29 22:37:44 +0400

    Constants for transactions

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const contractABI = abi.abi;
