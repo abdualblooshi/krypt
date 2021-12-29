311cc70ebf6e03252f669c8e15ee01d5bd04b9b7
gitanimous <aboodshadow@outlook.com>
2021-12-29 22:01:24 +0400

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,15 @@
-import React from "react";
-import {
-  Navbar,
-  Welcome,
-  Footer,
-  Loader,
-  Transactions,
-  Services,
-} from "./components";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-import "./index.css";
-
-const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-};
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
